81e5e7e3c055378771db403c0a739bff441ce05a
AbdealiJK <abdealikothari@gmail.com>
2016-02-10 13:22:45 +0530

    coala-json: Add new arg --text-logs
    coala-json normally returns the logs as JSON. Using
    this arg, the user can select to see the logs in the normal
    text format. This helps because the user doesnt have
    to wait for the analysis to complete before seeing the
    logs on their terminal.
    
    Fixes https://github.com/coala-analyzer/coala/issues/847

--- coalib/tests/coalaJSONTest.py
+++ coalib/tests/coalaJSONTest.py
@@ -56,3 +56,10 @@ class coalaJSONTest(unittest.TestCase):
         retval, output = execute_coala(coala_json.main, 'coala-json', '-v')
         self.assertEquals(retval, 0)
         self.assertEquals(output.strip(), VERSION)
+
+    def test_text_logs(self):
+        retval, output = execute_coala(
+            coala_json.main, 'coala-json', '--text-logs', '-c', 'nonex')
+        self.assertRegex(
+            output,
+            ".*\\[ERROR\\].*The requested coafile '.*' does not exist.\n")
